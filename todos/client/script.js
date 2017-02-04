4d672c68a2f25546498a8f92ef213eb31b3a3a4b
Lynnette Ng <lynnette-ng@outlook.com>
2017-02-04 03:32:50 +0000

    Link AJAX

--- todos/client/script.js
+++ todos/client/script.js
@@ -1,7 +1,38 @@
 var $ = require('jquery');
 
 $(function(){
+   $(':button').on('click', addTodo);
+   
+   $(':text').on('keypress', function(e){
+      var key = e.keyCode;
+      if (key == 13 || key == 169) {
+         addTodo();
+         e.preventDefault();
+         e.stopPropagation();
+         return false;
+      }
+   });
+   
    $('input').on('click', function(){
        $(this).parent().toggleClass('checked');
    }); 
 });
+
+var addTodo = function() {
+  var text = $('#add-todo-text').val();
+  
+  $.ajax({
+     url: '/api/todos',
+     type: 'POST',
+     data: {
+        text: text
+     },
+     dataType: 'json',
+     success: function(data) {
+        var todo = data.todo;
+        var newLiHtml = '<li><input type="checkbox"><span> ' + todo + '</span></li>';
+        $('form + ul').append(newLiHtml);
+        $('#add-todo-text').val('');
+     }
+  });
+};
